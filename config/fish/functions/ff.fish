eb744165f5a4d69030c7b1fae954f3631583d99a
newird <newird@protonmail.com>
2023-10-02 20:37:41 +0800

    add ff which combine fd and fzf     rff which combine rg and fzf

--- config/fish/functions/ff.fish
+++ config/fish/functions/ff.fish
@@ -1,3 +1,8 @@
 function ff
-    fzf --print0 | xargs -0 -o vim
+    fd -t f --hidden |
+        fzf --ansi \
+            --color "hl:-1:underline,hl+:-1:underline:reverse" \
+            --delimiter : \
+            --preview 'bat --color=always {1} ' \
+            --bind 'enter:become(nvim {1})'
 end
