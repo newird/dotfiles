ae449248e81a499c2f88837de520cabe7a95c9a4
newird <newird@protonmail.com>
2024-01-24 13:59:05 +0800

    change to hyprland and eww

---
+++ config/eww/scripts/workspaces.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+print_workspaces() {
+    buf=""
+    n=10
+    focused=$(hyprctl activeworkspace -j | jq '.id')
+    for (( i=1; i<=$n; i++ )) do
+        occupied=$(hyprctl workspaces -j | jq ".[] | select(.id == $i and .windows > 0)")
+        if [ "$focused" == "$i" ]; then
+            icon="●"
+            class="focused"
+        elif [[ $occupied ]]; then
+            icon="◉"
+            class="occupied"
+        else
+            icon="○"
+            class="empty"
+        fi
+        buf="$buf (eventbox :cursor \"hand\" (button :class \"$class\" :onclick \"hyprctl dispatch workspace $i\" \"$icon\"))"
+    done
+    echo "(box :class \"workspaces\" :spacing 10 :halign \"center\" :valign \"center\" :vexpand true $buf)"
+}
+
+print_workspaces
+socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+    print_workspaces
+done
