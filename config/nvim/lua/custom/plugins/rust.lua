2eda00f04317d91b73ac3e745100e99742591dd4
newird <newird@protonmail.com>
2024-10-27 19:07:13 +0800

    add desc for crate nvim

--- config/nvim/lua/custom/plugins/rust.lua
+++ config/nvim/lua/custom/plugins/rust.lua
@@ -41,18 +41,73 @@ return {
       require('crates').setup()
 
       local crates = require('crates')
-      local opts = { silent = true }
-      vim.keymap.set('n', '<leader>cgt', crates.toggle, opts)
-      vim.keymap.set('n', '<leader>cgr', crates.reload, opts)
-      vim.keymap.set('n', '<leader>cgv', crates.show_versions_popup, opts)
-      vim.keymap.set('n', '<leader>cgf', crates.show_features_popup, opts)
-      vim.keymap.set('n', '<leader>cgd', crates.show_dependencies_popup, opts)
-      vim.keymap.set('n', '<leader>cgu', crates.update_crate, opts)
-      vim.keymap.set('n', '<leader>cge', crates.expand_plain_crate_to_inline_table, opts)
-      vim.keymap.set('n', '<leader>cgH', crates.open_homepage, opts)
-      vim.keymap.set('n', '<leader>cgR', crates.open_repository, opts)
-      vim.keymap.set('n', '<leader>cgD', crates.open_documentation, opts)
-      vim.keymap.set('n', '<leader>cgC', crates.open_crates_io, opts)
+      vim.keymap.set(
+        'n',
+        '<leader>cgt',
+        crates.toggle,
+        { silent = true, desc = 'Toggle crates popup' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cgr',
+        crates.reload,
+        { silent = true, desc = 'Reload crates popup' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cgv',
+        crates.show_versions_popup,
+        { silent = true, desc = 'Show versions popup' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cgf',
+        crates.show_features_popup,
+        { silent = true, desc = 'Show features popup' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cgd',
+        crates.show_dependencies_popup,
+        { silent = true, desc = 'Show dependencies popup' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cgu',
+        crates.update_crate,
+        { silent = true, desc = 'Update crate' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cge',
+        crates.expand_plain_crate_to_inline_table,
+        { silent = true, desc = 'Expand plain crate to inline table' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cgH',
+        crates.open_homepage,
+        { silent = true, desc = 'Open homepage' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cgR',
+        crates.open_repository,
+        { silent = true, desc = 'Open repository' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cgD',
+        crates.open_documentation,
+        { silent = true, desc = 'Open documentation' }
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>cgC',
+        crates.open_crates_io,
+        { silent = true, desc = 'Open crates.io' }
+      )
     end,
   },
 }
+
